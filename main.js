6e8c9e1445e08da42aac760ffda3e5bc05fb4af2
Micah Caffey <mrcaffey@gmail.com>
2018-08-23 00:23:17 -0600

    restaurant layout

---
+++ main.js
@@ -0,0 +1,9 @@
+
+
+
+$(document).ready(function () {
+  $('address').each(function () {
+     var link = "<a href='http://maps.google.com/maps?q=" + encodeURIComponent( $(this).text() ) + "' target='_blank'>" + $(this).text() + "</a>";
+     $(this).html(link);
+  });
+});
