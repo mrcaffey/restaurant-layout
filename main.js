0e3b8746010a192058f14478f22f530889609e7d
Micah Caffey <mrcaffey@gmail.com>
2018-09-27 21:09:14 -0600

    outlined page comments

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 
 
-
+//Google Maps Click for Location
 $(document).ready(function () {
   $('address').each(function () {
      var link = "<a href='http://maps.google.com/maps?q=" + encodeURIComponent( $(this).text() ) + "' target='_blank'>" + $(this).text() + "</a>";
      $(this).html(link);
   });
 });
+
+//Click to Call Phone Number
+
